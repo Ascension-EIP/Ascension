3fa5d18c5482f7f5f79ee010fa7e9d5a33bbee5f
DimitriLaPoudre <lou.pellegrino@epitech.eu>
2026-03-12 11:07:50 +0100

    feat(server): delete old server

--- apps/server.ref/internal/transport/http/dto/request/auth.go
+++
@@ -1,8 +0,0 @@
-package request
-
-type (
-	AuthForm struct {
-		Username string `json:"username" binding:"required,min=3,max=32"`
-		Password string `json:"password" binding:"required,min=8"`
-	}
-)
--- apps/server.ref/internal/transport/http/dto/response/user.go
+++
@@ -1,9 +0,0 @@
-package response
-
-type User struct {
-	ID        string `json:"id"`
-	Username  string `json:"username"`
-	Type      string `json:"type"`
-	CreatedAt string `json:"created_at"`
-	UpdatedAt string `json:"updated_at"`
-}
