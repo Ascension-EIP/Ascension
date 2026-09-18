12a936ae8df38e0592f692e567535ea0fc854ed5
Gianni TUERO <gianni.tuero@epitech.eu>
2026-09-18 15:18:11 +0900

    refactor: update TODO comments for job processing and payload schema in pose_detect_request

--- apps/new_ai/src/worker/handlers/pose_detect_request.py
+++ apps/new_ai/src/worker/handlers/pose_detect_request.py
@@ -16,14 +16,16 @@ from worker import broker, pose_skeleton
 @payload_validity
 def pose_detect_request(ch, method, properties, payload):
     log.info(f" [x] POSE_DETECT_REQUEST: {payload}")
+    # TODO: créer le schema pour la paylaod quej e suis censé recevoir
     job_id = payload.get("job_id")
-    # TODO: traiter le job ici, puis acquitter le message.
     broker.publish(RoutingKey.POSE_DETECT_STATUS, JobStatus(job_id=job_id, status=JobStatus.PENDING))
     try:
+        # TODO: avoir le lien s3 et downlaod temporairement la vidéo pour la process
         pose_skeleton.process()
         broker.publish(RoutingKey.POSE_DETECT_COMPLETED, Job(job_id=job_id))
         broker.publish(RoutingKey.POSE_DETECT_STATUS, JobStatus(job_id=job_id, status=JobStatus.SUCCESS))
-        # TODO:
+        # TODO: créer le schéma representant celui de la db pour les résultats
+        # TODO: mettre dans la db les résultats, que le worker ira fetch après avec ce job id ou autre
     except Exception:  # noqa: BLE001
         broker.publish(RoutingKey.POSE_DETECT_STATUS, JobStatus(job_id=job_id, status=JobStatus.FAILED, message=traceback.format_exc()))
         log.error(traceback.format_exc())
