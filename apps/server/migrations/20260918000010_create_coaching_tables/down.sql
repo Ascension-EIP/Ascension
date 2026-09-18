3d8ac34cd6005a86c4813a7b7b98f8dbf6856d27
Christophe Vandevoir <christophe.vandevoir@epitech.eu>
2026-09-18 17:56:36 +0900

    feat(database): add coaching and training tables

---
+++ apps/server/migrations/20260918000010_create_coaching_tables/down.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS training_logs;
+DROP TABLE IF EXISTS exercises;
+DROP TABLE IF EXISTS training_program_sessions;
+DROP TABLE IF EXISTS training_programs;
+DROP TABLE IF EXISTS goals;
